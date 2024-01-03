4328e14d7ec684ea94037fd4fb78d356a643d5c4
emadam <ldy9037@naver.com>
2024-01-03 09:58:58 +0900

    feat: 파일 복구/삭제 로직 추가
    - restore() : 휴지통 내 파일들 복구
    - delete() : Filtering 된 파일들을 휴지통으로 이동

--- main.py
+++ main.py
@@ -10,25 +10,46 @@ from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 FILTER = "[bMtMPqp].stop"
 
-def main():
-    stack = []
-
-    drives = get_dirves()
-    for drive in drives:
-        #if drive['name'] == 'GSO':
-          stack.append(drive['id'])
-
-          while stack:
-            folder_id = stack.pop()
-            files = get_files(drive['id'], folder_id)
-            for file in files:
-              print(f"파일 : {file['name']} ({file['id']})") 
-
-            folders = get_folders(drive['id'], folder_id)
-            for folder in folders:
-              print(f"폴더 : {folder['name']}")
-              stack.append(folder['id'])
-
+def restore():
+  drives = get_drives()
+  
+  page_token = None
+  for drive in drives:
+    while True:    
+      files = get_deleted_files(drive['id'], page_token)
+      
+      if not page_token:
+        page_token = files['nextPageToken']
+      
+      if not files['files']:
+          break
+        
+      for file in files['files']:
+          print(f"{file['name']}")
+          restore_files(drive['id'], file['id'])
+          
+def delete():
+  drives = get_drives()
+  count = 0
+  
+  page_token = None
+  for drive in drives:
+    while True:    
+      files = get_files(drive['id'], page_token)
+      
+      if not page_token:
+        page_token = files['nextPageToken']
+      
+      if not files['files']:
+          break
+        
+      for file in files['files']:
+          print(f"{file['name']}")
+          deleted_file = delete_files(drive['id'], file['id'])
+          count += 1
+          
+  print("총 삭제된 파일 수 : " + count)
+        
 def _credentials():
   """Shows basic usage of the Drive v3 API.
   Prints the names and ids of the first 10 files the user has access to.
@@ -49,7 +70,7 @@ def _credentials():
   
   return creds
 
-def get_dirves():
+def get_drives():
   result = []
   
   try:
@@ -58,7 +79,6 @@ def get_dirves():
     results = (
         service.drives()
         .list(
-            q="name = 'GSO'",
             fields="nextPageToken, drives(id, name)"
         )
         .execute()
@@ -70,8 +90,49 @@ def get_dirves():
 
   return result
 
-def get_files(drive_id, folder_id):
-    result = []
+def restore_files(drive_id ,file_id):
+  try:
+      service = build("drive", "v3", credentials=_credentials())
+
+      results = (
+          service.files()
+          .update(
+              fileId=file_id,
+              supportsAllDrives=True,
+              body={
+                'trashed' : False
+              }
+          )
+          .execute()
+      )
+
+  except HttpError as error:
+      print(f"An error occurred: {error}")
+
+def delete_files(drive_id ,file_id):
+  try:
+      service = build("drive", "v3", credentials=_credentials())
+
+      results = (
+          service.files()
+          .update(
+              fileId=file_id,
+              supportsAllDrives=True,
+              body={
+                'trashed' : True
+              }
+          )
+          .execute()
+      )
+
+  except HttpError as error:
+      print(f"An error occurred: {error}")
+
+def get_files(drive_id, page_token):
+    result = {
+      'files' : [],
+      'nextPageToken' : None
+    }
 
     try:
         service = build("drive", "v3", credentials=_credentials())
@@ -79,24 +140,30 @@ def get_files(drive_id, folder_id):
         results = (
             service.files()
             .list(
-                q="'" + folder_id + "' in parents and trashed=false and name contains '" + FILTER + "'",
+                q="trashed=false and name contains '" + FILTER + "'",
                 corpora="drive",
+                pageSize=1000,
                 driveId=drive_id,
                 includeItemsFromAllDrives=True,
                 supportsAllDrives=True,
-                fields="files(id, name)"
+                pageToken=page_token,
+                fields="nextPageToken, files(id, name)"
             )
             .execute()
         )
-        result = results.get("files", [])
+        result['files'] = results.get("files", [])
+        result['nextPageToken'] = results.get("nextPageToken", None)
 
     except HttpError as error:
         print(f"An error occurred: {error}")
 
     return result
 
-def get_folders(drive_id, folder_id):
-  result = []
+def get_deleted_files(drive_id, pageToken):
+  result = {
+    'files' : [],
+    'nextPageToken' : None
+  }
 
   try:
       service = build("drive", "v3", credentials=_credentials())
@@ -104,16 +171,19 @@ def get_folders(drive_id, folder_id):
       results = (
           service.files()
           .list(
-              q="'" + folder_id + "' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false",
+              q="trashed=true",
               corpora="drive",
+              pageSize=1000,
               driveId=drive_id,
               includeItemsFromAllDrives=True,
               supportsAllDrives=True,
-              fields="files(id, name)"
+              pageToken=pageToken,
+              fields="nextPageToken, files(id, name)"
           )
           .execute()
       )
-      result = results.get("files", [])
+      result['files'] = results.get("files", [])
+      result['nextPageToken'] = results.get("nextPageToken", None)
 
   except HttpError as error:
       print(f"An error occurred: {error}")
@@ -121,4 +191,4 @@ def get_folders(drive_id, folder_id):
   return result
 
 if __name__ == "__main__":
-  main()
+  delete()
